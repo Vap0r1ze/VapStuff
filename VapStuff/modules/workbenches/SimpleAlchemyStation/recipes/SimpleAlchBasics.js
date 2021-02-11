8dde6d40fcb505ae867ea68d2226261d3725276a
Vap0r1ze <superdash993@gmail.com>
2021-02-11 14:31:08 +0000

    Deploying to dist from  @ cbfcb4e56d00e9b4c2a8fbabdadb3ee00b9c44dd 🚀

--- VapStuff/modules/workbenches/SimpleAlchemyStation/recipes/SimpleAlchBasics.js
+++ VapStuff/modules/workbenches/SimpleAlchemyStation/recipes/SimpleAlchBasics.js
@@ -3,26 +3,47 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const ItemStack_js_1 = require("../../../../../lib/org/bukkit/inventory/ItemStack.js");
 const Material_js_1 = require("../../../../../lib/org/bukkit/Material.js");
 const Module_js_1 = require("../../../../types/Module.js");
+const MinecraftData = require("minecraft-data");
 class SimpleAlchBasics extends Module_js_1.default {
     constructor() {
         super(...arguments);
         this.WORKBENCH_ID = 'simpleAlch';
     }
     onEnable() {
-        this.plugin.extraRecipes.addRecipe(this.WORKBENCH_ID, 'nameTag', {
-            name: 'Name Tag',
-            ingredients: [
-                [Material_js_1.default.LEAD, 1],
-                [Material_js_1.default.SLIME_BALL, 1],
-                [Material_js_1.default.WRITABLE_BOOK, 1],
-                [Material_js_1.default.LEATHER, 1],
-            ],
-            createResult: () => new ItemStack_js_1.default(Material_js_1.default.NAME_TAG),
-            waterUsage: 1,
-        });
+        this.addSimpleRecipe('leather', [
+            Material_js_1.default.BLAZE_POWDER,
+            Material_js_1.default.ROTTEN_FLESH,
+        ], Material_js_1.default.LEATHER);
+        this.addSimpleRecipe('slimeball', [
+            Material_js_1.default.WATER_BUCKET,
+            [Material_js_1.default.KELP_PLANT, 4],
+            [Material_js_1.default.LIME_DYE, 4],
+        ], Material_js_1.default.SLIME_BALL);
+        this.addSimpleRecipe('nameTag', [
+            Material_js_1.default.LEAD,
+            Material_js_1.default.SLIME_BALL,
+            Material_js_1.default.WRITABLE_BOOK,
+            Material_js_1.default.LEATHER,
+        ], Material_js_1.default.NAME_TAG);
     }
     onDisable() {
         this.plugin.extraRecipes.removeRecipe(this.WORKBENCH_ID, 'nameTag');
+    }
+    addSimpleRecipe(id, ingredients, result, waterUsage = 1) {
+        let amount = 1;
+        let mat;
+        if (Material_js_1.default.$isInstance(result)) {
+            mat = result;
+        }
+        else {
+            [mat, amount] = result;
+        }
+        this.plugin.extraRecipes.addRecipe(this.WORKBENCH_ID, 'nameTag', {
+            name: MinecraftData('1.16.5').itemsByName[`${mat.getKey()}`.replace('minecraft:', '')].displayName,
+            ingredients: ingredients.map(ingr => (Material_js_1.default.$isInstance(ingr) ? [ingr, 1] : ingr)),
+            createResult: () => new ItemStack_js_1.default(mat, amount),
+            waterUsage,
+        });
     }
 }
 exports.default = SimpleAlchBasics;
